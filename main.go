4b8bfea4281507e826d23bbb8f942c94246de0e3
zzWinD <gongqizhou@qq.com>
2016-11-17 22:13:45 +0800

    字符串

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var x uint8 = 1<<1 | 1<<5
 	var y uint8 = 1<<1 | 1<<2
-	// 2 | 32
-	//
 	fmt.Printf("%08b\n", x)
 	fmt.Printf("%08b\n", y)
-	fmt.Printf("%03b\n", 2)
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+	fmt.Printf("%08b\n", x&^y)
 
-	fmt.Printf("\n%b\n", 10)
-	fmt.Printf("%b\n", 1<<1)
-	fmt.Printf("%b\n", 1<<5)
+	fmt.Printf("%s %[1]s %[1]s", "你好")
+
+	s := "hello,world"
+
+	fmt.Println(s[0:5])
+
 }
