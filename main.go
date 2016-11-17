e6b8471e83ff335eff68eebf204007b306abfabd
zzWinD <zzwind@live.cn>
2016-11-17 16:59:48 +0800

    位操作

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	p := 0
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	// 2 | 32
+	//
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
+	fmt.Printf("%03b\n", 2)
 
-	pp := &p
-
-	*pp = 1
-
-	fmt.Println(p)
+	fmt.Printf("\n%b\n", 10)
+	fmt.Printf("%b\n", 1<<1)
+	fmt.Printf("%b\n", 1<<5)
 }
